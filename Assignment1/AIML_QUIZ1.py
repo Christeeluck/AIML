cd982213b1faabf684873c925b4b5227198003e4
rednova7 <christeeluck@gmail.com>
2024-03-10 13:59:21 -0400

    Update AIML_QUIZ1.py

--- Assignment1/AIML_QUIZ1.py
+++ Assignment1/AIML_QUIZ1.py
@@ -28,13 +28,29 @@ plt.grid()
 plt.show()
 
 #3 estimate coefficient of correlation r
+print(r)
 
 #4 predict spd of 13 yr old car using lin reg model
+Car_Age=13
+Speed_Predict= Car_Age *slope + intercept
+print(Speed_Predict)
 
 #5 3rd degree polynomial plot of data
+n=3
+mymodel=np.poly1d(np.polyfit(x,y,n))
+myline=np.linspace(1,17,100)
+plt.scatter(x,y)
+plt.plot(myline,mymodel(myline))
+plt.xlabel('Age of car')
+plt.ylabel('Speed of car')
+plt.grid()
+plt.show()
+
+
 
 #6 predict speed of 13 year old car using 3rd degree polynomial
-
+speed=mymodel(13)
+print(speed)
 #7 estimate the r-squared value 
 
 #8 difference between lin and poly prediction
